b5bfe39b9c86e3b0aab418c5c8a21ef923db45f7
Neha Menon <neha.menon221@gmail.com>
2024-04-04 17:09:05 -0400

    mark resolved debug

--- server.py
+++ server.py
@@ -425,7 +425,7 @@ def mark_resolved(post_id=None):
 	time_resolved_query = "UPDATE Post SET date_resolved = now() WHERE post_id = :post_id"
 	g.conn.execute(text(time_resolved_query), {'post_id': post_id})
 	g.conn.commit()
-	return redirect('/save_location')
+	return redirect('/user_location')
 
 
 @app.route('/add_location')
